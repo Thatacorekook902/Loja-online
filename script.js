7f34a550358765499e2acebc2c67c429f4cc33fd
Thainá Santos Alencar <167568065+Thatacorekook902@users.noreply.github.com>
2026-06-01 07:54:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function comprar(produto){
+async function comprar(produto, preco){
+     const resposta = await fetch("/checkouat", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json" 
+        },
+        body: JSON.stringify({
+            produto,
+            preco
+       })
+    })
+    const dados = await resposta.json()
+    const stripe = Stripe("Sua chave Publica")
+    stripe.redirectToCheckout({
+      sessionId: dados.id
+})
 
-alert(
-
-"Você comprou: " + produto
-
-)
 
 }
+    
